7f7541756dc83077823c355293963a5b6ab9120c
DatBoiEsteban <esteban1113001@gmail.com>
2019-08-18 13:15:46 -0600

    pi

---
+++ Assets/Scripts/Player/CreaturePicker.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreaturePicker : MonoBehaviour
+{
+    private GameObject picker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        picker = gameObject.transform.Find("Picker").gameObject;
+    }
+}
