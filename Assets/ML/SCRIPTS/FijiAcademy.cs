70d45735a196cf4747527fe25a7bdacc43be08d2
Carlos <carlosgomezsoza@gmail.com>
2019-08-18 02:02:45 -0600

    ml changes not moving

---
+++ Assets/ML/SCRIPTS/FijiAcademy.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class FijiAcademy : Academy
+{
+    public float agentRunSpeed;
+	public float agentRotationSpeed;
+	public float spawnAreaMarginMultiplier = 1;
+    public Material goalScoredMaterial;
+    public Material failMaterial;
+	public float gravityMultiplier;
+    public GameObject foodPrefab;
+    public float foodGeneartion;
+    public float food = 0;
+    public Bounds areaBounds;
+
+    public override void InitializeAcademy()
+    {
+        base.InitializeAcademy();
+        areaBounds = GetComponent<BoxCollider>().bounds;
+
+        //IEnumerator getHungryCoroutine = generateFood(0.1f);
+        //StartCoroutine(getHungryCoroutine);
+    }
+
+    public override void AcademyReset()
+    {
+    	
+    }
+    
+    private void FixedUpdate()
+    {
+        
+        food += foodGeneartion;
+        //Debug.Log(food);
+
+        if (food > 1)
+        {
+            Instantiate(foodPrefab, GetRandomSpawnPos(), Quaternion.identity);
+            food -= 1;
+        }
+    }
+    
+
+    private IEnumerator generateFood(float waitTime) {
+        while (true)
+        {
+            food += foodGeneartion* waitTime;
+
+            if (food > 1)
+            {
+                Instantiate(foodPrefab, GetRandomSpawnPos(), Quaternion.identity);
+                food -= 1;
+            }
+            yield return new WaitForSeconds(waitTime);
+            
+        }
+    }
+
+    public Vector3 GetRandomSpawnPos()
+    {
+        bool foundNewSpawnLocation = false;
+        Vector3 randomSpawnPos = Vector3.zero;
+
+        float randomPosX = Random.Range(-areaBounds.extents.x *spawnAreaMarginMultiplier,
+                                 areaBounds.extents.x * spawnAreaMarginMultiplier);
+
+        float randomPosZ = Random.Range(-areaBounds.extents.z * spawnAreaMarginMultiplier,
+                                             areaBounds.extents.z * spawnAreaMarginMultiplier);
+
+        //Debug.Log(-areaBounds.extents.x * spawnAreaMarginMultiplier + "---"+randomPosZ);
+        randomSpawnPos = transform.position + new Vector3(randomPosX, 1f, randomPosZ);
+        /*if (Physics.CheckBox(randomSpawnPos, new Vector3(2.5f, 0.01f, 2.5f)) == false)
+        {
+             foundNewSpawnLocation = true;
+        }*/
+
+        return randomSpawnPos;
+    }
+
+
+}
---
+++ Assets/SCRIPTS/FoodSource.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FoodSource : MonoBehaviour
+{
+    public float nutritionalValue = 1;
+    public string cosumerTag = "creature";
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Food");
+        //Debug.Log("food is being tocuhed!! " + collision.gameObject.tag + "/" + cosumerTag +(collision.gameObject.tag==co));
+        if (collision.gameObject.tag == cosumerTag) {
+            GameObject creature = collision.gameObject;
+            FijiAgent agent = creature.GetComponent<FijiAgent>();
+            agent.feed(nutritionalValue);
+            nutritionalValue = 0;
+            Destroy(gameObject);
+        }
+
+    }
+
+}
