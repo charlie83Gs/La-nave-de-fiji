c214faf2515274d03ee63a34f86438173e24ea5e
DatBoiEsteban <esteban1113001@gmail.com>
2019-08-18 16:22:39 -0600

    acutal

--- Assets/Scripts/Areas/AreaController.cs
+++ Assets/Scripts/Areas/AreaController.cs
@@ -24,7 +24,7 @@ public class AreaController : MonoBehaviour
             GameObject a = Instantiate(PlayerArea, new Vector3(AreaPos[0] * Area[PlayerAmount][0], 0.01f, AreaPos[1] * Area[PlayerAmount][1]), Quaternion.identity, this.transform);
             a.GetComponent<Renderer>().material.color = new Color(Random.Range(0f,1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
             a.transform.localScale = new Vector3(AreaPos[0]/5, 1, AreaPos[1]/5);
-            
+            a.GetComponent<AreaInfo>().playerNumber = PlayerAmount;
         }
     }
 }
