cf61205dd91041a87e95941b12b47ac8d8479d1f
DatBoiEsteban <esteban1113001@gmail.com>
2019-08-18 12:39:37 -0600

    Players

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -1,52 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
+using Luminosity.IO;
 
 public class PlayerControl : MonoBehaviour
 {
     private Rigidbody rb;
-    private PlayerControls controls;
-	private Vector2 move;
-    private Vector2 rotate;
-    private float speed = 1f;
+    private GlobalSettings settings;
 
-    public string player;
-    
+    public float speed = 1.5f;
+    public PlayerID player;
 
     private void Awake()
     {
-        controls = new PlayerControls();
-
-        controls.Player.Move.performed += ctx => move = ctx.ReadValue<Vector2>();
-        controls.Player.Move.canceled += ctx => move = Vector2.zero;
-
-        controls.Player.Aim.performed += ctx => rotate = ctx.ReadValue<Vector2>();
-
-        controls.Player.PickUpThrow.performed += ctx => Debug.Log(ctx);
-  
+        settings = GlobalSettings.Instance;
     }
-   
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        speed *= settings.settings.speedMultiplier;
     }
 
-    private void FixedUpdate()
+    private void Update()
     {
-        Vector3 m = new Vector3(move.x * speed, rb.velocity.y, move.y * speed);
-        rb.velocity = m;
-        Vector3 r = new Vector3(rotate.x, 0, rotate.y);
-        transform.rotation = Quaternion.LookRotation(r);
+        float hor = InputManager.GetAxis("HorizontalMove", player);
+        float ver = InputManager.GetAxis("VerticalMove", player);
+        if (0 != hor  || 0 != ver)
+        {
+            rb.MovePosition(transform.position + new Vector3(hor, 0, -ver) * Time.deltaTime);
+        }
+
+        float aimHor = InputManager.GetAxis("HorizontalAim", player);
+        float aimVer = InputManager.GetAxis("VerticalAim", player);
+        if(aimVer != 0 || aimHor != 0)
+        {
+            Vector3 r = new Vector3(aimHor, 0, -aimVer);
+            transform.rotation = Quaternion.LookRotation(r, Vector3.up);
+        }
     }
 
-    private void OnEnable()
-    {
-        controls.Player.Enable();
-    }
 
-    private void OnDisable()
-    {
-        controls.Player.Disable();
-    }
+
 }
--- Assets/Scripts/Settings/GlobalSettings.cs
+++ Assets/Scripts/Settings/GlobalSettings.cs
@@ -3,7 +3,8 @@
 public class GlobalSettings : Singleton<GlobalSettings>
 {
     public AreaSettings settings;
-
+    
+    protected GlobalSettings() { }
     private void Start()
     {
         settings = new AreaSettings();
