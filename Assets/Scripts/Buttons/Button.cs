beaf2812bd8c25767b409d7ea5674998619f9d74
Victor Castrillo <victor.castrillo.99@gmail.com>
2019-08-18 20:49:55 -0700

    User Interface update

---
+++ Assets/Scripts/Buttons/Button.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button : MonoBehaviour
+{
+    public void ChangeScene(int targetScene)
+    {
+        SceneManager.LoadScene(targetScene);
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Closing game...");
+        Application.Quit();
+    }
+}
